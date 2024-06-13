dc37b466c5d5165bd49b629217cf67fff74d6408
jangwoolee <8282qwe@gmail.com>
2024-06-13 10:33:39 +0900

    소셜 로그인

--- src/main/java/com/_6bitcampers/nangman_doctor/homeController.java
+++ src/main/java/com/_6bitcampers/nangman_doctor/homeController.java
@@ -1,13 +1,31 @@
 package com._6bitcampers.nangman_doctor;
 
+import com._6bitcampers.nangman_doctor.servingPackage.jangwoo.loginService.roleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 @Controller
+@RequiredArgsConstructor
 public class homeController {
+    private final roleService roleService;
 
     @GetMapping("/")
     public String home() {
-        return "home";
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return roleService.checkRole(role);
     }
 }
