8fd7b15a395c5d8c3bf40cea5b1e1bb844c1c29b
jangwoolee <8282qwe@gmail.com>
2024-06-18 12:01:12 +0900

    role 수정

--- src/main/java/com/_6bitcampers/nangman_doctor/homeController.java
+++ src/main/java/com/_6bitcampers/nangman_doctor/homeController.java
@@ -27,6 +27,6 @@ public class homeController {
         GrantedAuthority auth = iter.next();
         String role = auth.getAuthority();
 
-        return "home";
+        return roleService.checkRole(role);
     }
 }
