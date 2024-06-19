33d8ce98c37a4c230b23fd6c210735132f43b88c
dongwoobae <https://github.com/dongwoobae/naver0314devops11.git>
2024-06-19 15:17:23 +0900

    06 19 15 17

--- src/main/java/com/_6bitcampers/nangman_doctor/baedongwoo/controller/hospital/HospitalDetailController.java
+++ src/main/java/com/_6bitcampers/nangman_doctor/baedongwoo/controller/hospital/HospitalDetailController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +27,7 @@ public class HospitalDetailController {
             HttpSession session,
             @RequestParam int review_no,
             @RequestParam(required = false) String userId,
+            @RequestParam int currentPage,
             Model model) {
         //회원일 경우에는 이름으로 식별, 비회원일 경우에는 sessionId로 식별
         String identifier = (userId != null && !userId.equals("anonymousUser")) ? userId : request.getSession().getId();
@@ -55,6 +57,8 @@ public class HospitalDetailController {
         model.addAttribute("userDto",userDto);
         model.addAttribute("hospital_no",hospital_no);
         model.addAttribute("review_no",review_no);
+        model.addAttribute("userId",userId);
+        model.addAttribute("currentPage",currentPage);
 
         return "reviewdetail";
     }
@@ -64,7 +68,10 @@ public class HospitalDetailController {
             @RequestParam String review_title,
             @RequestParam int review_no,
             @RequestParam String review_content,
-            @RequestParam int review_likecount
+            @RequestParam int review_likecount,
+            @RequestParam String userId,
+            @RequestParam int currentPage,
+            Model model
     ){
         Map<String,Object>map=new HashMap<>();
         map.put("review_title",review_title);
@@ -73,7 +80,11 @@ public class HospitalDetailController {
         map.put("review_likecount",review_likecount);
         reviewService.updateReview(map);
 
-        return "redirect:/reviewboard/detail";
+       model.addAttribute("review_no",review_no);
+       model.addAttribute("userId",userId);
+       model.addAttribute("currentPage",currentPage);
+
+        return "forward:/reviewboard/detail";
     }
 
     @ResponseBody
