b4bd31d2894ad5255fa6643f2e12bd83b683dc4f
hayooniiiiii <140838571+hayooniiiiii@users.noreply.github.com>
2024-06-25 14:24:10 +0900

    마이페이지 수정삭제 ALERT뜨는거수정완

--- src/main/java/com/_6bitcampers/nangman_doctor/hayoon/controller/StatusController.java
+++ src/main/java/com/_6bitcampers/nangman_doctor/hayoon/controller/StatusController.java
@@ -4,73 +4,58 @@ import com._6bitcampers.nangman_doctor.hayoon.Dto.ReservationDto;
 import com._6bitcampers.nangman_doctor.hayoon.Mapper.ReservationMapper;
 import com._6bitcampers.nangman_doctor.hayoon.Service.ReservationService;
 import com._6bitcampers.nangman_doctor.hayoon.Service.StatusService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.thymeleaf.TemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-@AllArgsConstructor
 @Controller
+@RequiredArgsConstructor
 public class StatusController {
-    private  ReservationMapper reservationMapper;
-    private JavaMailSender mailSender;
-    private TemplateEngine templateEngine;
 
-
-    @Autowired
-    private StatusService statusService;
-
-    @Autowired
-    private ReservationService reservationService;
-
+    private final ReservationMapper reservationMapper;
+    private final StatusService statusService;
 
     @PostMapping("/SendStatus")
-    public Map<String, Object> updateReservationStatus(@RequestParam int reservationNo,@RequestParam String isAccepted, Model model) {
-
+    @ResponseBody
+    public Map<String, Object> updateReservationStatus(@RequestParam int reservationNo, @RequestParam String isAccepted) {
         Map<String, Object> response = new HashMap<>();
-        response.put("reservationNo", reservationNo);
-        response.put("isAccepted", isAccepted);
+        try {
+            response.put("reservationNo", reservationNo);
+            response.put("isAccepted", isAccepted);
 
-        System.out.println(reservationNo);
-        System.out.println(isAccepted);
-        model.addAttribute("reservationNo", reservationNo);
-        model.addAttribute("isAccepted", isAccepted);
-        if(isAccepted.equals("true")) {
-            statusService.updateStatus(reservationNo);
-            if(statusService.getStatus(reservationNo)==2){
-                statusService.updateRoom(UUID.randomUUID().toString(),reservationNo);
+            if ("true".equals(isAccepted)) {
+                statusService.updateStatus(reservationNo);
 
+                if (statusService.getStatus(reservationNo) == 2) {
+                    statusService.updateRoom(UUID.randomUUID().toString(), reservationNo);
 
-                // Get email addresses
-                String employeeEmail=reservationMapper.getEmployeemailByNo(reservationMapper.getEmployeeNo(reservationNo));
-                String userEmail=reservationMapper.getUsermailByNo(reservationMapper.getUserNoByres(reservationNo));
+                    String employeeEmail = reservationMapper.getEmployeemailByNo(reservationMapper.getEmployeeNo(reservationNo));
+                    String userEmail = reservationMapper.getUsermailByNo(reservationMapper.getUserNoByres(reservationNo));
 
-                System.out.println(employeeEmail);
-                System.out.println(userEmail);
-
-                // 예약 보내기
-                ReservationDto reservationDto=reservationMapper.getResdto(reservationNo);
-                statusService.sendReservationRequestEmail(userEmail, reservationDto);
-                statusService.sendReservationRequestEmail(employeeEmail, reservationDto);
-
-                System.out.println(reservationDto);
-
+                    ReservationDto reservationDto = reservationMapper.getResdto(reservationNo);
+                    statusService.sendReservationRequestEmail(userEmail, reservationDto);
+                    statusService.sendReservationRequestEmail(employeeEmail, reservationDto);
+                }
+            } else {
+                statusService.deleteByReservationNo(reservationNo);
             }
-        }
-        else{
-            statusService.deleteByReservationNo(reservationNo);
+            response.put("status", "success");
+        } catch (Exception e) {
+            System.err.println("Error updating reservation status: " + e.getMessage());
+            e.printStackTrace();
+            response.put("status", "error");
+            response.put("message", e.getMessage());
         }
 
         return response;
-
     }
+
     @GetMapping("/edit")
     public String editReservation(@RequestParam int reservationNo,Model model){
         int infoNo=statusService.getInfoNoByReservationNo(reservationNo);
@@ -88,9 +73,5 @@ public class StatusController {
         statusService.deleteByReservationNo(reservationNo);
         return "success";
     }
-
-
-
-
 
 }
