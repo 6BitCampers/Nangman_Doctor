7a213477be6cd1c86999307b48f721f906fc0761
andychoi0819 <andychoi0819@gmail.com>
2024-06-25 16:39:53 +0900

    6/25 사진 넣기랑 디폴트 등등

--- src/main/java/com/_6bitcampers/nangman_doctor/search/HospitalService.java
+++ src/main/java/com/_6bitcampers/nangman_doctor/search/HospitalService.java
@@ -5,22 +5,27 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
 @Service
 public class HospitalService {
 
     @Autowired
     private HospitalMapper hospitalMapper;
 
-    public List<HospitalDto> searchHospitals(String keyword) {
-        return hospitalMapper.findByNameContaining(keyword);
+    public List<HospitalDto> searchHospitals(String keyword, int page, int size) {
+        int offset = (page - 1) * size;
+        return hospitalMapper.findByNameContaining(keyword, size, offset);
     }
 
-    public List<HospitalDto> searchTopRatedHospitals(String keyword) {
-        return hospitalMapper.findTopRatedByKeyword(keyword).stream().limit(10).toList();
+    public List<HospitalDto> searchTopRatedHospitals(String keyword, int page, int size) {
+        int offset = (page - 1) * size;
+        return hospitalMapper.findTopRatedByKeyword(keyword, size, offset);
     }
+
+    public long countHospitals(String keyword) {
+        return hospitalMapper.countByNameContaining(keyword);
+    }
+
     public HospitalDto findHospitalById(Long hospitalId) {
         return hospitalMapper.findById(hospitalId);
     }
-
 }
